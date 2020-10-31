ee59f70064f630ce8e60e177f8acae34e3d1c75d
gentleatif <69183987+gentleatif@users.noreply.github.com>
2020-10-31 12:27:30 +0530

    ADD IT

---
+++ index.js
@@ -0,0 +1,75 @@
+
+                            /* adding event listener to button press */
+for(var i=0; i<document.querySelectorAll("button").length; i++){
+
+document.querySelectorAll("button")[i].addEventListener("click",function (){
+
+  var innerHTML= this.innerHTML
+             makesound(innerHTML);
+            buttonAnimation(innerHTML);
+
+});}
+
+                          /* adding avent listener to keyboard keypress  */
+document.addEventListener("keypress",function(e){
+
+makesound(e.key);
+buttonAnimation(e.key);
+
+});
+function makesound(key){
+  switch (key) {
+            case "w":
+          var tom1=new Audio('tom-1.mp3');
+              tom1.play();
+
+              break;
+
+            case "a":
+            var tom2=new Audio('tom-2.mp3');
+                tom2.play();
+              break;
+
+            case "s":
+            var tom3=new Audio('tom-3.mp3');
+                tom3.play();
+              break;
+
+              case "d":
+              var tom4=new Audio('tom-4.mp3');
+                  tom4.play();
+                break;
+             case "j":
+             var tom5=new Audio('snare.mp3');
+                 tom5.play();
+               break;
+
+               case "k":
+               var tom6=new Audio('crash.mp3');
+                 tom6.play();
+                 break;
+                  case "l":
+                 var tom7=new Audio('kick-bass.mp3');
+                     tom7.play();
+                   break;
+
+            default:console.log(innerHTML)    /* isse error dhundhne me asani hogi bichance agar koi dusra button click ho raha hain to pata chalega
+
+            */
+
+          }
+}
+
+function buttonAnimation(currentKey) {
+  var activeButton = document.querySelector("." + currentKey);
+   activeButton.classList.add("pressed");
+
+setTimeout(function(){
+activeButton.classList.remove("pressed");
+
+},100);
+
+
+
+
+}
